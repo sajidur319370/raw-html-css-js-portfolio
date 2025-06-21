9f57a8d3db09f6f1ca595a23bda67e157a8a7d52
sajidur319370 <sajidurrahmansahib@gmail.com>
2025-06-21 21:45:49 +0600

    add functionality to show every section of resume

--- js/script.js
+++ js/script.js
@@ -2,9 +2,14 @@
 const resumeButtons = document.querySelectorAll(".resume-button");
 resumeButtons.forEach((btn, idx) => {
   btn.addEventListener("click", () => {
+    const resumeDetails = document.querySelectorAll(".resume-detail");
     resumeButtons.forEach((btn) => {
       btn.classList.remove("active");
     });
     btn.classList.add("active");
+    resumeDetails.forEach((detail) => {
+      detail.classList.remove("active");
+    });
+    resumeDetails[idx].classList.add("active");
   });
 });
