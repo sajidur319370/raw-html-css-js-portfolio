a86974883b08c801c56b976371882133c2be57fd
sajidur319370 <sajidurrahmansahib@gmail.com>
2025-06-24 19:33:49 +0600

    add responsive css and more animation js done

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 const navLinks = document.querySelectorAll("header nav  a");
 const logoLink = document.querySelector(".logo");
 const sections = document.querySelectorAll("section");
+const menuIcon = document.querySelector("#menu-icon");
+const navBar = document.querySelector("header nav");
+menuIcon.addEventListener("click", () => {
+  menuIcon.classList.toggle("bx-x");
+  navBar.classList.toggle("active");
+});
 const activePage = () => {
   const header = document.querySelector("header");
   const barsBox = document.querySelector(".bars-box");
@@ -18,6 +24,8 @@ const activePage = () => {
   sections.forEach((section) => {
     section.classList.remove("active");
   });
+  menuIcon.classList.remove("bx-x");
+  navBar.classList.remove("active");
 };
 
 navLinks.forEach((link, idx) => {
